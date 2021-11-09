5078ef0332dce48a3fd3cff02d3264c4362647ec
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-11-09 21:08:35 +0530

    welcome

---
+++ src/main/java/com/parkinglot/ParkingLotSystem.java
@@ -0,0 +1,18 @@
+package com.parkinglot;
+
+public class ParkingLotSystem {
+    public ParkingLotSystem() {
+    }
+
+    /**
+     * this methode is for parking a vehicle
+     * @author pallavi
+     * @since 9-11-21
+     * @param vehicle
+     * @return
+     */
+    public boolean park(Object vehicle) {
+        return true;
+
+    }
+}
---
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -0,0 +1,14 @@
+package com.parkinglot;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ParkingLotTest {
+
+    @Test
+   public void givenVehicle_whenParked_shouldReturnTrue() {
+       ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+        boolean isparked = parkingLotSystem.park(new Object());
+        Assertions.assertTrue(isparked);
+    }
+}
