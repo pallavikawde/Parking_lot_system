a95170e4efb5093f6b3d8e09eba04e46564f1e1e
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-11-15 18:26:07 +0530

    Added: UC7_givenAVehicle_WhenParked_ThenCheckForPosition_ShouldReturnPosition

--- src/test/java/com/parkinglot/ParkingLotSystemTest.java
+++ src/test/java/com/parkinglot/ParkingLotSystemTest.java
@@ -149,18 +149,18 @@ public class ParkingLotSystemTest {
         attendant.parkedByAttendant(vehicle);
         Assertions.assertTrue(parkingLotSystem.isVehicleParked(vehicle));
     }
-////uc7
-//    @Test
-//    void givenAVehicle_WhenParked_ThenCheckForPosition_ShouldReturnPosition() throws ParkingLotSystemException {
-//        Vehicle vehicle1 = new Vehicle("CHEVROLET", "IN-MP0023", "11:00");
-//        Vehicle vehicle2 = new Vehicle("AUDI", "IN-KL003", "9:00");
-//        parkingLotSystem.park(vehicle1);
-//        parkingLotSystem.park(vehicle2);
-//        int vehiclePositionForVehicle1 = parkingLotSystem.getVehiclePosition(vehicle1);
-//        int vehiclePositionForVehicle2 = parkingLotSystem.getVehiclePosition(vehicle2);
-//        Assertions.assertEquals(0, vehiclePositionForVehicle1);
-//        Assertions.assertEquals(1, vehiclePositionForVehicle2);
-//    }
+//uc7
+    @Test
+    void givenAVehicle_WhenParked_ThenCheckForPosition_ShouldReturnPosition() throws ParkingLotSystemException {
+        Vehicle vehicle1 = new Vehicle("CHEVROLET", "IN-MP0023", "11:00");
+        Vehicle vehicle2 = new Vehicle("AUDI", "IN-KL003", "9:00");
+        parkingLotSystem.park(vehicle1);
+        parkingLotSystem.park(vehicle2);
+        int vehiclePositionForVehicle1 = parkingLotSystem.getVehiclePosition(vehicle1);
+        int vehiclePositionForVehicle2 = parkingLotSystem.getVehiclePosition(vehicle2);
+        Assertions.assertEquals(0, vehiclePositionForVehicle1);
+        Assertions.assertEquals(1, vehiclePositionForVehicle2);
+    }
 ////uc8
 //    @Test
 //    void givenAVehicle_WhenParked_ThenCheckTimeOfParking_ShouldReturnParkingTime() throws ParkingLotSystemException {
